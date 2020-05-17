53466bbc4534ef7cf0172d2b6751980376458241
m-atoms <m.adams@pitt.edu>
2020-05-16 17:30:38 -0700

    write matches to pretty html output file

--- seq_scan.py
+++ seq_scan.py
@@ -26,3 +26,65 @@ def itr(idx, seq):
             offset+=1
     
     return true_idx + offset
+
+# iterate over each character, build substrings incrementally,
+# then write substring to list when 10 uppercase chars have been found
+def split(num, seq_tagd):
+    seq_seg = []
+    ctr = 0
+    seg = ""
+    
+    for idx, char in enumerate(seq_tagd):
+        if char.isupper():
+            ctr+=1
+            if (ctr == num) or (idx == (len(seq_tagd)-1)):
+                ctr = 0
+                seg += char
+                seq_seg.append(seg)
+                seg = ""
+            else:
+                seg += char
+        elif (idx == (len(seq_tagd)-1)):
+            seg += char
+            seq_seg.append(seg)
+        else:
+            seg += char
+    return seq_seg
+
+# use hijacked stdout and print straight to output file
+# html doesn't care about indents or whitespace so neither do I
+# could this have been done more elegantly? yes, but it does the job and I'm tired
+def html_gen(seq_seg):
+    print("<html>")
+    print("<head>")
+    # define style
+    print("<style>")
+    print("* {color:white; font-family:'Courier New';}") #Lucida Console is another good monospace font
+    print("strong {color:red;}") # matches appear bold and red
+    print(".num {text-align:right;}") # numbers are aligned to the right of each cell
+    print(".seq {font-size:18px;}") # increase font from default (16px)
+    print("</style>")
+    print("</head>")
+    #define table
+    print("<body>")
+    print("<table>")
+    print("<tr>")
+    # first row of numbering
+    for i in range(0,5):
+        print("<td class='num'>{}</td>".format((i+1)*10))
+    print("</tr>")
+    # create table rows for sequence and corresponding numbers
+    print("<tr>")
+    for idx, seg in enumerate(seq_seg):
+        if idx > 0 and idx % 5 == 0:
+            print("</tr>")
+            print("<tr>")
+            for i in range(0,5):
+                print("<td class='num'>{}</td>".format((idx+(i+1))*10))
+            print("</tr>")
+            print("<tr>")
+        print("<td class='seq'>{}</td>".format(seg))
+    print("</tr>")
+    print("</table>")
+    print("</body>")
+    print("</html>")
